7f7b5a47a3a4eb971653a0c6ef32ecb4ecc6a2e2
Aleksa Savic <aleksasavicposao@gmail.com>
2022-11-20 21:53:27 +0100

    Make HwmonSensors just a subclass of SensorsTree

---
+++ hwmon.py
@@ -0,0 +1,134 @@
+import os
+
+from PyQt6 import QtCore
+
+from sensors_tree import SensorsTree, Device, Sensor
+
+
+class HwmonSensors(SensorsTree):
+    HWMON_PATH = "/sys/class/hwmon"
+
+    def __init__(self):
+        super().__init__()
+
+    class HwmonDevice(Device):
+        def __init__(self, name):
+            super().__init__(name)
+
+        class HwmonSensor(Sensor):
+            def __init__(self, label, internal_data):
+                super().__init__(label, internal_data)
+
+            def update_value(self):
+                with open(self._internal_data["path"]) as f:
+                    self._value = f.readline().strip()
+                    if self._value.isdecimal():
+                        self._value = int(self._value)
+                        self._min_value = min(self._value, self._min_value)
+                        self._max_value = max(self._value, self._max_value)
+                    else:
+                        self._min_value = "N/A"
+                        self._max_value = "N/A"
+
+                self._tree_item.setData(
+                    1,
+                    QtCore.Qt.ItemDataRole.DisplayRole,
+                    str(self._value),
+                )
+                self._tree_item.setData(
+                    2,
+                    QtCore.Qt.ItemDataRole.DisplayRole,
+                    str(self._min_value),
+                )
+                self._tree_item.setData(
+                    3,
+                    QtCore.Qt.ItemDataRole.DisplayRole,
+                    str(self._max_value),
+                )
+
+            def type(self):
+                internal_name = self._internal_data["fs_name"]
+
+                if "temp" in internal_name:
+                    return self.Type.Temp
+                elif "in" in internal_name:
+                    return self.Type.Voltage
+                elif "fan" in internal_name:
+                    return self.Type.Fan
+                elif "curr" in internal_name:
+                    return self.Type.Current
+                elif "power" in internal_name:
+                    return self.Type.Power
+                elif "intrusion" in internal_name:
+                    return self.Type.Intrusion
+
+            def get_icon(self):
+                return super().get_icon()
+
+            def get_tree_widget_item(self):
+                if self._value == 0:
+                    return None
+                return super().get_tree_widget_item()
+
+            def __str__(self):
+                if not type(self._value) == int:
+                    return self._value
+
+                if self.type == self.Type.Temp:
+                    divide_by = 1000
+                    unit = "C"
+                elif self.type == self.Type.Voltage:
+                    divide_by = 1000
+                    unit = "V"
+                elif self.type == self.Type.Fan:
+                    divide_by = 1
+                    unit = "RPM"
+                elif self.type == self.Type.Current:
+                    divide_by = 1000
+                    unit = "A"
+                elif self.type == self.Type.Power:
+                    divide_by = 1000000
+                    unit = "W"
+                else:
+                    divide_by = 1
+                    unit = ""
+
+                return f"{self._value / divide_by} {unit}"
+
+    def read(self):
+        hwmon_dirs = os.listdir(self.HWMON_PATH)
+        for subdir in hwmon_dirs:
+            subdir_path = os.path.join(self.HWMON_PATH, subdir)
+
+            with open(os.path.join(subdir_path, "name")) as f:
+                device_name = f.readline().strip()
+
+            h_device = self.HwmonDevice(device_name)
+            self.devices.append(h_device)
+
+            for sensor in os.listdir(subdir_path):
+                sensor_parts = sensor.split("_")
+                sensor_label_path = os.path.join(subdir_path, sensor_parts[0], "_label")
+                sensor_label = sensor_parts[0]
+                if os.path.exists(sensor_label_path):
+                    with open(sensor_label_path) as f:
+                        sensor_label = f.readline().strip()
+
+                sensor_path = os.path.join(subdir_path, sensor)
+                if os.path.isfile(sensor_path):
+                    if "_input" in sensor:
+                        with open(sensor_path) as f:
+                            try:
+                                internal_data = {
+                                    "fs_name": sensor,
+                                    "label": sensor_label,
+                                    "path": sensor_path,
+                                }
+
+                                h_sensor = self.HwmonDevice.HwmonSensor(
+                                    sensor_label, internal_data
+                                )
+                                h_sensor.update_value()
+                                h_device.sensors.append(h_sensor)
+                            except OSError:
+                                pass  # TODO: Show error
---
+++ sensors_tree.py
@@ -0,0 +1,102 @@
+import sys
+from abc import abstractmethod, ABC
+from enum import Enum
+
+from PyQt6 import QtGui
+from PyQt6.QtWidgets import QTreeWidgetItem, QTreeWidget
+
+
+class SensorsTree(ABC):
+    def __init__(self):
+        self.devices = list()
+
+    def update_sensors(self):
+        for device in self.devices:
+            for sensor in device.sensors:
+                sensor.update_value()
+
+    def get_tree_widget(self):
+        tree_widget = QTreeWidget()
+        tree_widget.setColumnCount(4)
+        tree_widget.setHeaderLabels(["Name", "Value", "Min", "Max"])
+        for device in self.devices:
+            device_item = QTreeWidgetItem([device.name])
+            for sensor in device.sensors:
+                device_item.addChild(sensor.get_tree_widget_item())
+            tree_widget.addTopLevelItem(device_item)
+
+        return tree_widget
+
+    @abstractmethod
+    def read(self):
+        raise NotImplementedError()
+
+
+class Device(ABC):
+    def __init__(self, name):
+        self.name = name
+        self.sensors = list()
+
+
+class Sensor(ABC):
+    class Type(Enum):
+        Temp = 1
+        Voltage = 2
+        Fan = 3
+        Current = 4
+        Power = 5
+        Intrusion = 6
+
+    def __init__(self, label, internal_data):
+        self.label = label
+        self._internal_data = internal_data
+
+        self._value = 0
+        self._min_value = sys.maxsize
+        self._max_value = -sys.maxsize - 1
+
+        self._tree_item = QTreeWidgetItem(
+            [
+                label,
+                str(self._value),
+                str(self._min_value),
+                str(self._max_value),
+            ]
+        )
+
+        self._tree_item.setIcon(0, QtGui.QIcon(f"icons:{self.get_icon()}"))
+
+    @abstractmethod
+    def update_value(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def type(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_icon(self):
+        feat_icon = None
+        if self.type() == self.Type.Temp:
+            feat_icon = "icons8-thermometer-96.png"
+        elif self.type() == self.Type.Current:
+            feat_icon = "icons8-high-voltage-96.png"
+        elif self.type() == self.Type.Voltage:
+            feat_icon = "icons8-voltmeter-100.png"
+        elif self.type() == self.Type.Power:
+            feat_icon = "icons8-shutdown-90.png"
+        elif self.type() == self.Type.Fan:
+            feat_icon = "icons8-fan-head-64.png"
+        elif self.type() == self.Type.Intrusion:
+            feat_icon = "icons8-hips-100.png"
+
+        return feat_icon
+
+    @abstractmethod
+    def __str__(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_tree_widget_item(self):
+        return self._tree_item
